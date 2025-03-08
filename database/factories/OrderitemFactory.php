9c8b4f9eac542454155fc9280441b93b0ad9b892
chillakhon <shoev-03@mail.ru>
2025-03-08 14:45:26 +0500

    Temporary commit

--- database/factories/OrderitemFactory.php
+++ database/factories/OrderitemFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Orderitem>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\OrderItem>
  */
-class OrderitemFactory extends Factory
+class OrderItemFactory extends Factory
 {
     /**
      * Define the model's default state.
