3f7bd3da343919efb83e977af563095cbcc7cb70
sb-dor <avaz.shams.2002@gmail.com>
2025-04-28 05:33:20 +0500

    made some enhancement

--- app/Http/Controllers/Api/Admin/ProductController.php
+++ app/Http/Controllers/Api/Admin/ProductController.php
@@ -133,17 +133,18 @@ class ProductController extends Controller
         foreach ($products as &$product) {
             $productKey = "Product_{$product->id}";
 
-            if (isset($product['variants']) && !empty($product['variants'])) {
+            $product->inventory_balance = 0.0; // ✅ Initialize to 0
+
+            if (!empty($product['variants'])) {
                 foreach ($product['variants'] as &$variant) {
                     $variantKey = "ProductVariant_{$variant->id}";
                     $variant_total_qty = $inventory_balances[$variantKey]['total_quantity'] ?? 0.0;
                     $variant->inventory_balance = $variant_total_qty;
                     $product->inventory_balance += $variant_total_qty;
                 }
-                continue;
+            } else {
+                $product->inventory_balance = $inventory_balances[$productKey]['total_quantity'] ?? 0.0;
             }
-
-            $product->inventory_balance = $inventory_balances[$productKey]['total_quantity'] ?? 0.0;
         }
 
         return response()->json($products);
