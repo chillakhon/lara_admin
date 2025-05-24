34e4ba72e7f55b175be94d70b46c2eb5fc871809
sb-dor <avaz.shams.2002@gmail.com>
2025-05-24 20:52:45 +0500

    commit closes #222

--- app/Http/Controllers/Api/Admin/CountriesController.php
+++ app/Http/Controllers/Api/Admin/CountriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Country;
+use App\Models\Region;
 use Illuminate\Http\Request;
 
 class CountriesController extends Controller
@@ -28,6 +29,24 @@ class CountriesController extends Controller
 
     public function regions(Request $request)
     {
+        $regions = Region::query();
+
+        if ($request->get('name')) {
+            $regions->where("name", "like", "%{$request->get('name')}%");
+        }
+
+        if (!is_null($request->integer('country_id'))) {
+            $regions->where("country_id", $request->integer('country_id'));
+        }
+
+        $regions = $regions
+            ->orderBy('name', 'asc')
+            ->get();
+
+        return response()->json([
+            'success' => true,
+            'regions' => $regions,
+        ]);
     }
 
     public function cities(Request $request)
--- routes/api.php
+++ routes/api.php
@@ -138,7 +138,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
     Route::prefix('/countries')->group(function () {
         Route::get('/', [CountriesController::class, 'countries']);
-        Route::get('/regions', []);
+        Route::get('/regions', [CountriesController::class, 'regions']);
         Route::get('/cities', []);
     });
 
