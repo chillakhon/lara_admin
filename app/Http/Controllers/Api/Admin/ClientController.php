1ad6cfcafd392a2a4d7a7c1894e3dcb74a2b060b
sb-dor <avaz.shams.2002@gmail.com>
2025-06-01 18:06:14 +0500

    commit closes #274

--- app/Http/Controllers/Api/Admin/ClientController.php
+++ app/Http/Controllers/Api/Admin/ClientController.php
@@ -20,17 +20,14 @@ class ClientController extends Controller
 
     public function index(Request $request)
     {
-        $query = Client::with(['user.profile', 'level'])
+        $query = Client::with(['level'])
             ->withCount('orders')
             ->when($request->search, function ($query, $search) {
                 $query->where(function ($q) use ($search) {
-                    $q->whereHas('user.profile', function ($q) use ($search) {
+                    $q->whereHas('profile', function ($q) use ($search) {
                         $q->where('first_name', 'like', "%{$search}%")
                             ->orWhere('last_name', 'like', "%{$search}%");
-                    })
-                        ->orWhereHas('user', function ($q) use ($search) {
-                            $q->where('email', 'like', "%{$search}%");
-                        });
+                    })->orWhere('email', 'like', "%{$search}%");
                 });
             })
             ->when($request->level, function ($query, $level) {
@@ -39,14 +36,10 @@ class ClientController extends Controller
             ->when($request->status, function ($query, $status) {
                 switch ($status) {
                     case 'active':
-                        $query->whereHas('user', function ($q) {
-                            $q->whereNotNull('email_verified_at');
-                        });
+                        $query->whereNotNull('email_verified_at');
                         break;
                     case 'inactive':
-                        $query->whereHas('user', function ($q) {
-                            $q->whereNull('email_verified_at');
-                        });
+                        $query->whereNull('email_verified_at');
                         break;
                 }
             })
@@ -61,19 +54,16 @@ class ClientController extends Controller
             ->through(function ($client) {
                 return [
                     'id' => $client->id,
-                    'user' => [
-                        'id' => $client?->user?->id,
-                        'email' => $client?->user?->email,
-                        'email_verified_at' => $client?->user?->email_verified_at,
-                    ],
+                    'email' => $client?->email,
+                    'verified_at' => $client?->verified_at,
                     'profile' => [
-                        'first_name' => $client?->user?->profile?->first_name,
-                        'last_name' => $client?->user?->profile?->last_name,
-                        'full_name' => $client?->user?->profile?->full_name,
-                        'phone' => $client?->user?->profile?->phone,
+                        'first_name' => $client?->profile?->first_name,
+                        'last_name' => $client?->profile?->last_name,
+                        'full_name' => $client?->profile?->full_name,
+                        'phone' => $client?->profile?->phone,
                     ],
-                    'phone' => $client->phone,
-                    'address' => $client->address,
+                    'phone' => $client?->profile?->phone,
+                    'address' => $client?->profile?->address,
                     'bonus_balance' => $client->bonus_balance,
                     'level' => $client->level,
                     'orders_count' => $client->orders_count,
@@ -114,35 +104,30 @@ class ClientController extends Controller
             'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:8',
             'phone' => 'nullable|string|max:255',
             'address' => 'nullable|string|max:255',
-            'password' => 'required|string|min:8',
             'level_id' => 'nullable|exists:client_levels,id',
             'bonus_balance' => 'nullable|numeric|min:0',
         ]);
 
         $client = DB::transaction(function () use ($validated) {
-            $user = User::create([
+            $client = Client::create([
                 'email' => $validated['email'],
                 'password' => Hash::make($validated['password']),
-                'email_verified_at' => now(),
             ]);
 
-            $user->profile()->create([
+            $client->profile()->create([
                 'first_name' => $validated['first_name'],
                 'last_name' => $validated['last_name'],
                 'phone' => $validated['phone'],
-            ]);
-
-            $clientRole = Role::where('slug', 'client')->first();
-            $user->roles()->attach($clientRole);
-
-            return $user->client()->create([
-                'phone' => $validated['phone'],
                 'address' => $validated['address'],
-                'client_level_id' => $validated['level_id'],
-                'bonus_balance' => $validated['bonus_balance'] ?? 0,
             ]);
+
+            // $clientRole = Role::where('slug', 'client')->first();
+            // $user->roles()->attach($clientRole);
+
+            return $client->load('profile');
         });
 
         return response()->json([
@@ -163,36 +148,32 @@ class ClientController extends Controller
 
         DB::transaction(function () use ($validated, $client) {
             // 1) Обновляем профиль
-            $client->user->profile()->update([
+            $client->profile()->update([
                 'first_name' => $validated['first_name'],
                 'last_name' => $validated['last_name'],
-                'phone' => $validated['phone'],
-            ]);
-
-            // 2) Обновляем поля в clients
-            $client->update([
                 'phone' => $validated['phone'],
                 'address' => $validated['address'],
             ]);
 
+
             // 3) Обновляем почту пользователя
-            $client->user()->update([
+            $client->update([
                 'email' => $validated['email'],
             ]);
         });
 
         return response()->json([
             'message' => 'Клиент успешно обновлён',
-            'client' => $client->fresh()->load('user.profile'),
+            'client' => $client->fresh()->load('profile'),
         ]);
     }
 
 
     public function destroy(Client $client)
     {
-        $user = $client->user;
+        // $user = $client->user;
         $client->delete();
-        $user->delete();
+        // $user->delete();
 
         return response()->json([
             'message' => 'Клиент успешно удалён'
@@ -203,7 +184,7 @@ class ClientController extends Controller
     {
         // Эйджир-загружаем профиль пользователя вместе с другими связями
         $client->load([
-            'user.profile',                         // <-- добавили
+            'profile',                         // <-- добавили
             'orders' => function ($query) {
                 $query->latest();
             },
