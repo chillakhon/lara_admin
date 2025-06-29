c009549d8664d6fb7b33b70966190e182ec851ed
sb-dor <avaz.shams.2002@gmail.com>
2025-06-29 14:12:09 +0500

    added functions of: add_item_to_cart, add_multiple_items_to_cart, sync

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -5,15 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cart extends Model
 {
     use HasFactory;
 
-    protected $guarded = false;
+    protected $table = "cart";
+
+    protected $guarded = ['id'];
 
     public function client(): BelongsTo
     {
         return $this->belongsTo(Client::class);
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(CartItem::class);
     }
 }
