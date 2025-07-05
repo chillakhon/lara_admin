cfa8c2e3227fb3c3a843209edcda5fe0e9493817
sb-dor <avaz.shams.2002@gmail.com>
2025-07-05 21:07:41 +0500

    added schedule command for deactivating disconts which closes #367

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Console\Commands\CheckDiscountsValidity;
 use DefStudio\Telegraph\Models\TelegraphBot;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
@@ -7,6 +8,11 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('discounts:check-validity', function () {
+    $check_discount_validity = new CheckDiscountsValidity();
+    $check_discount_validity->handle();
+})->purpose('Activate and deactivate discounts')->everyMinute();
 
 Artisan::command('bot_settings', function () {
     $bot = TelegraphBot::first();
