92d39766f29992c27ad71e194bb4439523f3d62a
chillakhon <shoev-03@mail.ru>
2025-09-02 16:13:54 +0500

    latest

--- public/phpinfo.php
+++ public/phpinfo.php
@@ -1,3 +1,2 @@
 <?php
 phpinfo();
-echo "<?php echo 'post_max_size: ' . ini_get('post_max_size') . '<br>'; echo 'upload_max_filesize: ' . ini_get('upload_max_filesize'); ?>" | sudo tee /var/www/laravel/public/check.php
