2de17946d699ae03b34630f1aac2536c4b54b3ca
chillakhon <shoev-03@mail.ru>
2025-11-12 11:45:34 +0500

    mail_chat_integration

--- whatsapp-service/index.js
+++ whatsapp-service/index.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
