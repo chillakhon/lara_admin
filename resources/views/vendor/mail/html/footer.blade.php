ada9a2a05c0183c8080d280a10db4b9df6cb3f75
chillakhon <shoev-03@mail.ru>
2025-11-18 15:10:42 +0500

    add_margin_bottom

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -15,7 +15,7 @@
         <table align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
             <tr>
                 <td style="padding: 30px 0 0 0; border-top: 1px solid #ddd;">
-                    <p style="font-size: 12px; color: #999; margin: 20px 0 0 0; text-align: center;">
+                    <p style="font-size: 12px; color: #999; margin: 20px 0 20px 0; text-align: center;">
                         <a href="{{ url('/api/public/unsubscribe') }}" style="color: #0066cc; text-decoration: none;">
                             Отписаться от рассылки
                         </a>
